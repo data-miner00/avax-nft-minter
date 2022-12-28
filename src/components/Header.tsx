879927ddf5ebb098442c39dd0094682da440f914
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-28 10:20:00 +0800

    feat: App stylings and display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import ThemeSwitcher from "./ThemeSwitcher";
 import LanguageSwitcher from "./LanguageSwitcher";
+import WalletManager from "./WalletManager";
 
 function Header(): JSX.Element {
   const { t } = useTranslation();
@@ -11,22 +12,47 @@ function Header(): JSX.Element {
   return (
     <header className="">
       <div className="max-w-[1400px] mx-auto md:px-8 items-center flex justify-between h-24">
-        <div className="font-bold uppercase text-2xl text tracking-wide">
-          React + Esbuild
+        <div className="text-2xl text tracking-wide flex items-center">
+          <svg
+            className="block"
+            width="35"
+            height="35"
+            viewBox="0 0 80 80"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_301_2)">
+              <circle cx="40" cy="40" r="40" fill="#E75555" />
+              <rect x="44" y="39" width="15" height="15" fill="white" />
+              <rect x="26" y="21" width="15" height="15" fill="white" />
+              <rect x="44" y="21" width="15" height="15" fill="white" />
+              <rect
+                x="17.5"
+                y="29.5"
+                width="32"
+                height="32"
+                fill="white"
+                stroke="#E75555"
+                strokeWidth="3"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_301_2">
+                <rect width="80" height="80" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+
+          <span className="block ml-3 font-mono">
+            Aiotter<span className="font-bold">FI</span>
+          </span>
         </div>
-        <nav className="flex">
-          <Link to="/" className="px-4 font-semibold">
-            {t("home")}
-          </Link>
-          <Link to="/features" className="px-4 font-semibold">
-            {t("features")}
-          </Link>
-          <Link to="/about" className="px-4 font-semibold">
-            {t("about")}
-          </Link>
+
+        <div className="flex items-center">
+          <WalletManager />
           <ThemeSwitcher />
           <LanguageSwitcher />
-        </nav>
+        </div>
       </div>
     </header>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,66 +1,117 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect, useContext } from "react";
+import PinataClient from "../utils/pinataClient";
+import { getVariable, Variable } from "../utils/getVariable";
+import { configuraTokenURI, mintNFT as MT } from "../utils/ethersFacade";
+import { AppContext } from "../context/AppContext";
 
-import StackCard from "../components/StackCard";
-import techData from "../data/TechnologyData.json";
-import { useTranslation } from "react-i18next";
+type NFTMetadata = {
+  tokenId: string;
+  transactionId: string;
+  ipfsLink: string;
+};
 
 function Home(): JSX.Element {
-  const { t } = useTranslation();
+  const pinataClient = new PinataClient(
+    getVariable(Variable.REACT_APP_PINATA_API_KEY),
+    getVariable(Variable.REACT_APP_PINATA_API_SECRET)
+  );
+  const [selectedImage, setSelectedImage] = useState<Blob | null>();
+  const [mintedNFTs, setMintedNFTs] = useState<Array<NFTMetadata>>([]);
+  const [NFTName, setNFTName] = useState<string>("");
+  const [NFTDescription, setNFTDescription] = useState<string>("");
+  const { contract } = useContext(AppContext);
+
+  useEffect(() => {
+    console.log(mintedNFTs);
+  }, [mintedNFTs]);
+
+  async function mintNFT() {
+    // if (selectedImage) {
+    //   const uploadResult = await pinataClient.uploadImage(
+    //     URL.createObjectURL(selectedImage),
+    //     "custom-name"
+    //   );
+    //   const tokenURI = configuraTokenURI(
+    //     uploadResult.imageHash,
+    //     NFTName,
+    //     NFTDescription
+    //   );
+    //   const tx = await MT(tokenURI, contract);
+    //   setMintedNFTs((_mintedNFTs) => [
+    //     ..._mintedNFTs,
+    //     {
+    //       tokenId: tx.nonce,
+    //       transactionId: tx.hash,
+    //       ipfsLink: uploadResult.imageHash,
+    //     },
+    //   ]);
+    // }
+  }
 
   return (
-    <main className="">
-      <header className="mt-4 mb-16">
-        <motion.h1
-          className="my-awesome-h1"
-          initial={{
-            opacity: 0,
-            y: "100%",
-          }}
-          animate={{
-            animationDuration: "500ms",
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            type: "tween",
-          }}
-        >
-          {t("template_title")}
-        </motion.h1>
-        <motion.p
-          initial={{
-            opacity: 0,
-            y: "100%",
-          }}
-          animate={{
-            animationDuration: "500ms",
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            ease: "easeOut",
-            delay: 0.3,
-            type: "tween",
-          }}
-          className="text-center text-gray-500 dark:text-gray-300"
-        >
-          {t("template_description")}
-        </motion.p>
-      </header>
+    <div className="max-w-[1400px] mx-auto md:px-8 md:py-8">
+      <h1 className="text-3xl font-semibold text-center mb-20">
+        Upload an image to be minted
+      </h1>
 
-      <div className="mx-auto w-2/3 flex flex-wrap gap-7 justify-center items-center">
-        {techData.map((data, index) => (
-          <StackCard
-            key={index}
-            delay={index}
-            title={data.title}
-            description={t(data.title)}
-            url={data.url}
-          />
-        ))}
+      <div className="bg-gray-700 rounded-md h-96 w-[40rem] mx-auto grid place-items-center">
+        {selectedImage ? (
+          <div className="max-w-md">
+            <img
+              className="mb-4 max-h-80"
+              alt="image"
+              src={URL.createObjectURL(selectedImage)}
+            />
+          </div>
+        ) : (
+          <div className="bg-gray-500 rounded-lg">
+            <input
+              className=""
+              type="file"
+              accept=".jpg,.png,.jpeg,.webp"
+              name="uploadImage"
+              onChange={(event) => {
+                console.log(event.target?.files?.item(0));
+                setSelectedImage(event.target?.files?.[0]);
+              }}
+            />
+          </div>
+        )}
       </div>
-    </main>
+
+      {!selectedImage && (
+        <div className="flex flex-col items-center">
+          <div className="flex text-sm">
+            <button
+              className="p-2 bg-red-600 rounded-xl min-w-[80px]"
+              onClick={() => setSelectedImage(null)}
+            >
+              Remove
+            </button>
+            <button
+              className="p-2 ml-2 bg-green-600 rounded-xl min-w-[80px]"
+              onClick={mintNFT}
+            >
+              Mint
+            </button>
+          </div>
+          <div>
+            <input
+              className="block px-3 py-2 mb-2 rounded dark:bg-gray-700 dark:placeholder:text-white outline-1 outline-slate-600 focus:outline"
+              type="text"
+              onChange={(event) => setNFTName(event.target.value)}
+              placeholder="NFT Name"
+            />
+            <input
+              className="block px-3 py-2 rounded dark:bg-gray-700 dark:placeholder:text-white outline-1 outline-slate-600 focus:outline"
+              type="text"
+              onChange={(event) => setNFTDescription(event.target.value)}
+              placeholder="Description"
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
