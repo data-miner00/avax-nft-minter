dca8e98b0f3c8def957a32b9e929aff2b603121f
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-28 10:13:58 +0800

    chore: Add new typings

--- src/react-app.d.ts
+++ src/react-app.d.ts
@@ -1,3 +1,7 @@
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.png";
+
+declare module "@metamask/jazzicon" {
+  export default function (diameter: number, seed: number): HTMLElement;
+}
