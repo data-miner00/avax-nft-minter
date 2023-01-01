678f88730c65e9fa4920ad8b203bacd2fc0aded8
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-02 02:47:14 +0800

    feat: Drag and drop or click uploadable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -101,9 +101,38 @@ function Home(): JSX.Element {
                   />
                 </div>
               ) : (
-                <div className="bg-gray-500 rounded-lg">
+                <>
+                  <label
+                    onDragOver={(event) => {
+                      event.preventDefault();
+                      event.stopPropagation();
+                    }}
+                    onDrop={(event) => {
+                      event.preventDefault();
+                      event.stopPropagation();
+                      setSelectedImage(event.dataTransfer?.files?.[0]);
+                    }}
+                    htmlFor="image-upload"
+                    className="flex flex-col items-center justify-center cursor-pointer rounded-lg bg-gray-600 hover:bg-gray-500 h-80 w-[38rem]"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="36"
+                      height="36"
+                      fill="currentColor"
+                      className="block"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                    </svg>
+                    <span className="block mt-5 text-gray-300">
+                      Drag and drop or click to upload
+                    </span>
+                  </label>
                   <input
-                    className=""
+                    id="image-upload"
+                    className="hidden"
                     type="file"
                     accept=".jpg,.png,.jpeg,.webp"
                     name="uploadImage"
@@ -112,7 +141,7 @@ function Home(): JSX.Element {
                       setSelectedImage(event.target?.files?.[0]);
                     }}
                   />
-                </div>
+                </>
               )}
             </div>
 
